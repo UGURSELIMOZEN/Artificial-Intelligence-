02bda909cb68087ddc59b94890f0163e3e53e1a7
Uğur Selim Özen <72982905+UGURSELIMOZEN@users.noreply.github.com>
2021-05-28 16:23:04 +0300

    Create RandomLogSinTan.rkt

---
+++ Dr.Racket Complete Course/RandomLogSinTan.rkt
@@ -0,0 +1,9 @@
+#lang racket
+(sin 30)
+(- (sin 45) (+  (- 12 4)  (+  -7.1  34)) (cos 45)   )
+(- (sqrt 45) (sqr 3) (log 10)(random 12))
+(+ (random 12) (random 2))
+(random 5)
+(random 5)
+(random 5)
+(tan pi)
